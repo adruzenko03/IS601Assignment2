c104c8c60bf75c6e0dc70a39cbaa156c95d247cc
Alex <adruzenko03@gmail.com>
2024-10-31 12:41:00 -0400

    Complete string reverse

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function swapView(index){
     questionArr[index].classList.remove("hide")
     curr=index
 }
+
+function reverseString(){
+    let inp=document.getElementById("inp1").value
+    let ret=""
+    for(let i=inp.length-1;i>=0;i--){
+    ret=ret.concat(inp[i])
+    }
+
+    document.getElementById("out1").innerText=ret
+}
+
